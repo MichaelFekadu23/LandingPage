fa6c5039008ac321a0a992fd1c3fe456028152b1
Natnael <natnaelfekadu801@gmail.com>
2025-10-27 22:07:22 +0300

    responsive (v2)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -100,19 +100,19 @@ export default function Hero() {
           min-h-[55svh] sm:min-h-[60svh] xl:min-h-[75svh]
           px-4 sm:px-6 xl:px-24 2xl:px-36
           pt-[8vh] md:pt-[0vh] 2xl:pt-[12vh]
-          transition-opacity duration-300
-          ${isPlaying ? "opacity-100" : "opacity-0 pointer-events-none select-none"}
         `}
-        aria-hidden={!isPlaying}
       >
         <div className="mx-auto max-w-[32rem] text-center md:mx-0 md:text-left">
-          <h1 className="text-white text-[30px] sm:text-[36px] xl:text-[58px] font-nexabold leading-tight drop-shadow-md">
-            Ride for Less
-          </h1>
+          <div aria-hidden={!isPlaying} className={`transition-opacity duration-300
+          ${isPlaying ? "opacity-100" : "opacity-0 pointer-events-none select-none"}`}>
+            <h1 className="text-white text-[30px] sm:text-[36px] xl:text-[58px] font-nexabold leading-tight drop-shadow-md">
+              Ride for Less
+            </h1>
 
-          <p className="mt-3 font-nexa text-white/70 text-[16px] sm:text-[18px] xl:text-[24px] leading-[150%] drop-shadow-sm md:max-w-[378px] mx-auto md:mx-0">
-            Ride instantly, negotiate fares, or let Moto’s smart bidding find your best deal
-          </p>
+            <p className="mt-3 font-nexa text-white/70 text-[16px] sm:text-[18px] xl:text-[24px] leading-[150%] drop-shadow-sm md:max-w-[378px] mx-auto md:mx-0">
+              Ride instantly, negotiate fares, or let Moto’s smart bidding find your best deal
+            </p>
+          </div>
 
           <div className="flex justify-center md:justify-start">
             <GetTheAppButton />
