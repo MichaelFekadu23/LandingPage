91c3f87ac139d43ec5f799a2b1e4d9b81c24c007
Michael Fekadu <101578729+MichaelFekadu23@users.noreply.github.com>
2025-10-27 21:51:40 +0300

    Restore original font configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,7 +18,7 @@ module.exports = {
         nexa: ['NexaTrial', 'sans-serif'],
         nexabold: ['Nexa', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
-        normalInter: ['Inter', 'sans-serif']
+        normalInter: ['NormalInter']
       },
 
       boxShadow: {
