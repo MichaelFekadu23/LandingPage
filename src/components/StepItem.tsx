0aa026d32baef29c2a6cbac13cb20dd408003364
Natnael <natnaelfekadu801@gmail.com>
2025-11-02 23:56:02 +0300

    how

--- src/components/StepItem.tsx
+++ src/components/StepItem.tsx
@@ -20,12 +20,12 @@ export const StepItem: React.FC<Step> = ({ labelTop, labelBottom, iconSrc, iconN
         <span className="">
           {iconNode ? iconNode : iconSrc ? (
             // eslint-disable-next-line @next/next/no-img-element
-            <img
-              src={iconSrc}
-              alt=""
+            <object
+              data={iconSrc}
+              aria-label="Step icon"
+              type="image/svg+xml" 
               className="h-8 w-8 md:h-10 md:w-10 xl:h-auto xl:w-auto"
-              loading="lazy"
-              decoding="async"
+
             />
           ) : (
             <PlaceholderIcon />
